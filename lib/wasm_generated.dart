b3a505f99069b438e5b560970801659b0ca3e803
Maan2003 <manmeetmann2003@gmail.com>
2022-07-24 14:34:00 +0530

    [wip] wasm

---
+++ lib/wasm_generated.dart
@@ -0,0 +1,81 @@
+@JS()
+library web;
+
+import "package:js/js.dart";
+import "package:js/js_util.dart" show promiseToFuture;
+
+@JS()
+class WasmBridge {
+  // @Ignore
+  external get client;
+  external set client(v);
+  external get db;
+  external set db(v);
+  external String decodeInvoice(String invoice);
+}
+
+@JS("WasmBridge")
+abstract class _WasmBridge {
+  external Promise<void> JS$_init();
+  external Promise<bool> init();
+  external Promise<void> joinFederation(String configUrl);
+  external Promise<void> leaveFederation();
+  external Promise<num> balance();
+  external Promise<String> invoice(num amount);
+  external Promise<String> pay(String bolt11);
+}
+
+extension WasmBridgeExtensions on WasmBridge {
+  Future<void> JS$_init() {
+    final Object t = this;
+    final _WasmBridge tt = t as _WasmBridge;
+    return promiseToFuture(tt.JS$_init());
+  }
+
+  Future<bool> init() {
+    final Object t = this;
+    final _WasmBridge tt = t as _WasmBridge;
+    return promiseToFuture(tt.init());
+  }
+
+  Future<void> joinFederation(String configUrl) {
+    final Object t = this;
+    final _WasmBridge tt = t as _WasmBridge;
+    return promiseToFuture(tt.joinFederation(configUrl));
+  }
+
+  Future<void> leaveFederation() {
+    final Object t = this;
+    final _WasmBridge tt = t as _WasmBridge;
+    return promiseToFuture(tt.leaveFederation());
+  }
+
+  Future<num> balance() {
+    final Object t = this;
+    final _WasmBridge tt = t as _WasmBridge;
+    return promiseToFuture(tt.balance());
+  }
+
+  Future<String> invoice(num amount) {
+    final Object t = this;
+    final _WasmBridge tt = t as _WasmBridge;
+    return promiseToFuture(tt.invoice(amount));
+  }
+
+  Future<String> pay(String bolt11) {
+    final Object t = this;
+    final _WasmBridge tt = t as _WasmBridge;
+    return promiseToFuture(tt.pay(bolt11));
+  }
+}
+
+@JS()
+external WasmBridge get wasmBridge;
+
+@JS()
+abstract class Promise<T> {
+  external factory Promise(
+      void executor(void resolve(T result), Function reject));
+  external Promise then(void onFulfilled(T result), [Function onRejected]);
+}
+
---
+++ web/index.js
@@ -0,0 +1,64 @@
+import initWasm, { WasmClient, WasmDb, init_, decode_invoice } from "./pkg/minimint_bridge.js";
+// start loading wasm as soon as possible
+const wasmPromise = initWasm();
+export class WasmBridge {
+    constructor() {
+        this.client = undefined;
+        // TODO: use this for more than one federations
+        this.db = undefined;
+    }
+    async _init() {
+        this.db = await WasmDb.load("fedimint");
+        document.addEventListener("visibilitychange", () => {
+            // save database on hidden visibility
+            if (document.visibilityState == "hidden") {
+                // NOTE: no need to await, js promises execute without await
+                this.db.save();
+            }
+        });
+        // db.clone(), yay rust like js
+        this.client = await init_(this.db.clone());
+    }
+    // return true if user has joined federation
+    async init() {
+        return this.client !== undefined;
+    }
+    async joinFederation(configUrl) {
+        this.client = await WasmClient.join_federation(this.db.clone(), configUrl);
+    }
+    async leaveFederation() {
+        // TODO
+    }
+    async balance() {
+        return this.client.balance();
+    }
+    decodeInvoice(invoice) {
+        return decode_invoice(invoice);
+    }
+    async invoice(amount) {
+        return await this.client.invoice(amount);
+    }
+    async pay(bolt11) {
+        return await this.client.pay(bolt11);
+    }
+}
+async function main() {
+    await wasmPromise;
+    globalThis.wasmBridge = new WasmBridge();
+    // flutter is kind of weird
+    await globalThis.wasmBridge._init();
+}
+const mainPromise = main();
+async function load() {
+    await mainPromise;
+    // Download main.dart.js
+    console.log("loading");
+    const appRunnerPromise = globalThis._flutter.loader.loadEntrypoint({
+        serviceWorker: {
+            serviceWorkerVersion: globalThis.serviceWorkerVersion,
+        }
+    }).then(engineInitializer => engineInitializer.initializeEngine());
+    const appRunner = await appRunnerPromise;
+    await appRunner.runApp();
+}
+window.addEventListener('load', load);
