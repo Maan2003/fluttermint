ebdbfc47e2dc919423a9478d35747c203cdbb477
Justin Moon <mail@justinmoon.com>
2022-08-24 17:18:42 -0500

    Attempt to fix WASM build

--- lib/ffi.dart
+++ lib/ffi.dart
@@ -24,14 +24,14 @@ class MinimintClientImpl implements MinimintClient {
           : DynamicLibrary.open(_dylib));
 
   /// If this returns Some, user has joined a federation. Otherwise they haven't.
-  Future<bool> init() {
-    return api.init();
+  Future<bool> init() async {
+    final userDir = await getApplicationDocumentsDirectory();
+    return api.init(path: userDir.path);
   }
 
   Future<void> joinFederation({required String configUrl}) async {
-    await api.joinFederation(
-        userDir: (await getApplicationDocumentsDirectory()).path,
-        configUrl: configUrl);
+    final userDir = await getApplicationDocumentsDirectory();
+    await api.joinFederation(configUrl: configUrl, userDir: userDir.path);
   }
 
   Future<void> leaveFederation() {
